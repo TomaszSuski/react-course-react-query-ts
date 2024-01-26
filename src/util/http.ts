5d68e1989b0bfc8c0c1ecf2eadeac425076c320d
Tomasz Suski <tomasz.suski@orba.co>
2024-01-26 12:21:39 +0100

    fetchEvents function outsourced to separate file

---
+++ src/util/http.ts
@@ -0,0 +1,18 @@
+import { ErrorElement } from "../components/Events/NewEventsSection";
+
+export async function fetchEvents() {
+  const response = await fetch("http://localhost:3000/events");
+
+  if (!response.ok) {
+    const error = new Error(
+      "An error occurred while fetching the events"
+    ) as ErrorElement;
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { events } = await response.json();
+
+  return events;
+}
